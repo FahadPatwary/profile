f544018a8dbd9abed19283beb33a5a70db9f2fee
Fahad Patwary <65324420+FahadPatwary@users.noreply.github.com>
2021-09-26 08:48:36 +0600

    images added

---
+++ NewProfile.js
@@ -0,0 +1,139 @@
+[
+    {
+      id: 1,
+      Profile: One.png,
+      Name: "James Robert",
+      Age: 24,
+      Skills: "HTML,CSS,JS,React,Nodejs",
+      Education: "BSC",
+      Salary: 34000
+    },
+    {
+      id: 2,
+      Name: "John Thomas",
+      Age: 26,
+      Skills: "HTML,CSS,Python",
+      Education: "HSC",
+      Salary: 45000
+    },
+    {
+      id: 3,
+      Name: "Richard Charles",
+      Age: 21,
+      Skills: "HTML,CSS,JS,React,Vue",
+      Education: "SSC",
+      Salary: 36000
+    },
+    {
+      id: 4,
+      Name: "Matthew Anthony",
+      Age: 25,
+      Skills: "HTML,CSS,JS,React,Angular",
+      Education: "BSC",
+      Salary: 25000
+    },
+    {
+      id: 5,
+      Name: "Steven Andrew",
+      Age: 35,
+      Skills: "HTML,CSS,JS,Nodejs,Python",
+      Education: "CSE",
+      Salary: 75000
+    },
+    {
+      id: 6,
+      Name: "Joshua Kenneth",
+      Age: 25,
+      Skills: "HTML,CSS,JS,React,Java",
+      Education: "JSC",
+      Salary: 45000
+    },
+    {
+      id: 7,
+      Name: "George Ronald",
+      Age: 21,
+      Skills: "HTML,CSS,JS,Laravel",
+      Education: "PHD",
+      Salary: 50000
+    },
+    {
+      id: 8,
+      Name: "Timothy Jason",
+      Age: 18,
+      Skills: "HTML,CSS,JS,React,C",
+      Education: "BSC",
+      Salary: 65000
+    },
+    {
+      id: 9,
+      Name: "Jacob Gary",
+      Age: 19,
+      Skills: "HTML,CSS,JS,Java",
+      Education: "BSC",
+      Salary: 36000
+    },
+    {
+      id: 10,
+      Name: "Eric Stephen",
+      Age: 18,
+      Skills: "HTML,CSS,JS,React,Python",
+      Education: "BSC",
+      Salary: 40000
+    },
+    {
+      id: 11,
+      Name: "Brandon Samuel",
+      Age: 36,
+      Skills: "HTML,CSS,JS,Nodejs",
+      Education: "BSC",
+      Salary: 45000
+    },
+    {
+      id: 12,
+      Name: "Frank Jerry",
+      Age: 27,
+      Skills: "HTML,CSS,JS,React,Ruby",
+      Education: "HSC",
+      Salary: 46000
+    },
+    {
+      id: 13,
+      Name: "Nathan Zachary",
+      Age: 33,
+      Skills: "HTML,CSS,JS,Go",
+      Education: "Diploma",
+      Salary: 70000
+    },
+    {
+      id: 14,
+      Name: "Elon Gray",
+      Age: 21,
+      Skills: "HTML,CSS,JS,React,Fortan",
+      Education: "HSC",
+      Salary: 60000
+    },
+    {
+      id: 15,
+      Name: "Peter Jetro",
+      Age: 32,
+      Skills: "HTML,CSS,JS,React,Swift",
+      Education: "CSE",
+      Salary: 120000
+    },
+    {
+      id: 16,
+      Name: "Jyle John",
+      Age: 41,
+      Skills: "HTML,CSS,JS,Prolog",
+      Education: "HSC",
+      Salary: 90000
+    },
+    {
+      id: 17,
+      Name: "Jose Samuel",
+      Age: 26,
+      Skills: "HTML,CSS,JS,React,Perl",
+      Education: "HSC",
+      Salary: 32000
+    }
+  ]
